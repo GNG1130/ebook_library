9aa4dda4e6fc01c070cb44c612ae2ac3be003c03
呂文傑 <wenjie@lvwenjiedeMacBook-Air.local>
2024-06-02 22:56:51 +0800

    set sql to ebook

--- ebook_library/settings.py
+++ ebook_library/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'ebook_library.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default='postgresql://postgres:postgres@localhost:5432/ebook_library',
         conn_max_age=600
     )
 }
